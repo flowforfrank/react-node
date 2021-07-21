31212d88818a0c70f671cac2ff81a46affa4a322
flowforfrank <hello@allma.si>
2021-07-21 09:51:51 +0200

    🔥 Get rid of CORS

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express'),
       path    = require('path'),
-      cors    = require('cors'),
       route   = require('./route.js'),
       app     = express(),
       port    = process.env.PORT || 8080;
       
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.use(cors());
 
 route(app);
 
